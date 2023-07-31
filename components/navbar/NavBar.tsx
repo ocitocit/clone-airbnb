998c8ac2841c31f1b3a72f11283e79874310685d
Abdul Rosyid <ocritocrit@gmail.com>
2023-07-31 13:59:44 +0700

    add a nav component

---
+++ components/navbar/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "../Container";
+
+const NavBar = () => {
+  return (
+    <div className="fixed z-10 w-full bg-white shadow-sm">
+      <div className="border-b-[1px] py-4">
+        <Container></Container>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
