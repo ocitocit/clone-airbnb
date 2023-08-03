ce09d9e20f17f920f17ce95243146ca33e4c0a7d
Abdul Rosyid <ocritocrit@gmail.com>
2023-08-03 07:01:46 +0700

    change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import NavBar from "@/components/navbar/NavBar";
+import { Nunito } from "next/font/google";
 import "./globals.css";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Airbnb",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={nunito.className}>
         <NavBar />
         {children}
       </body>
