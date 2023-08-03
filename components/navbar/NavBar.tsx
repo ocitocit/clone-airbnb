43d20fb8ac8007f4dbec8926c93efee0041ba1f5
Abdul Rosyid <ocritocrit@gmail.com>
2023-08-03 07:41:57 +0700

    add some styles

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,12 +1,22 @@
-
-import React from "react";
 import Container from "../Container";
 
 const NavBar = () => {
   return (
     <div className="fixed z-10 w-full bg-white shadow-sm">
       <div className="border-b-[1px] py-4">
-        <Container>anythings</Container>
+        <Container>
+          <div
+            className="
+            flex
+            flex-row
+            items-center
+            justify-between
+            gap-3
+            md:gap-0"
+          >
+            logo
+          </div>
+        </Container>
       </div>
     </div>
   );
