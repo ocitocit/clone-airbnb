0a5080c2caf77924f1149bb67bc40a3749315dc0
Abdul Rosyid <ocritocrit@gmail.com>
2023-08-03 07:36:27 +0700

    styling container

--- components/Container.tsx
+++ components/Container.tsx
@@ -3,7 +3,11 @@ interface ContainerProps {
 }
 
 const Container: React.FC<ContainerProps> = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="mx-auto max-w-[2520px] px-4 sm:px-2 md:px-10 xl:px-20">
+      {children}
+    </div>
+  );
 };
 
 export default Container;
