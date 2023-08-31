fdcbcee6a6f5b7fe7259e738dae8a4cd65aa2da9
Temux Ocit <ocritocrit@gmail.com>
2023-08-31 14:47:15 +0700

    serch bar styling

--- components/navbar/Search.tsx
+++ components/navbar/Search.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { BiSearch } from "react-icons/bi"
+
 const Search = () => {
   return (
     <div
@@ -8,6 +10,7 @@ const Search = () => {
       w-full
       md:w-auto
       rounded-full
+      py-2
       shadow-sm
       hover:shadow-md
       transition
@@ -30,6 +33,44 @@ const Search = () => {
           "
         >
           Anywhere
+        </div>
+        <div
+          className="
+          hidden
+          sm:block
+          text-sm
+          font-semibold
+          px-6
+          border-x-[1px]
+          flex-1
+          text-center
+          "
+        >
+          Any Week
+        </div>
+        <div
+          className="
+          text-sm
+          pl-6
+          pr-2
+          text-gray-600
+          flex
+          flex-row
+          items-center
+          gap-3
+          "
+        >
+          <div className="hidden sm:block"> Add Guest</div>
+          <div
+            className="
+            p-2
+            bg-rose-500
+            rounded-full
+            text-white
+            "
+          >
+            <BiSearch />
+          </div>
         </div>
       </div>
     </div>
