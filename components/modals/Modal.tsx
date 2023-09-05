296cab2495991fa2ed73fda81de5eaf1e43815f4
Temux Ocit <ocritocrit@gmail.com>
2023-09-05 18:30:26 +0700

    create a modal component

---
+++ components/modals/Modal.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import React, { useState } from "react";
+
+interface ModalProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: boolean;
+  secondaryAction?: () => void;
+  secondaryLabel?: string;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+  onSubmit,
+  title,
+  body,
+  footer,
+  actionLabel,
+  disabled,
+  secondaryAction,
+  secondaryLabel
+}) => {
+  return (
+    <div></div>
+  )
+}
+
+export default Modal
