05032865e8475bd756f8983a24b038f3b97f1c47
Abdul Rosyid <ocritocrit@gmail.com>
2023-09-07 06:13:44 +0700

    start styling modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,20 @@
-import NavBar from "@/components/navbar/NavBar";
-import { Nunito } from "next/font/google";
-import "./globals.css";
+import NavBar from '@/components/navbar/NavBar';
+import { Nunito } from 'next/font/google';
+import './globals.css';
+import Modal from '@/components/modals/Modal';
 
-const nunito = Nunito({ subsets: ["latin"] });
+const nunito = Nunito({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Airbnb",
-  description: "Clone Airbnb",
+  title: 'Airbnb',
+  description: 'Clone Airbnb'
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={nunito.className}>
+        <Modal isOpen />
         <NavBar />
         {children}
       </body>
--- components/modals/Modal.tsx
+++ components/modals/Modal.tsx
@@ -1,6 +1,6 @@
-"use client"
+'use client';
 
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from 'react';
 
 interface ModalProps {
   isOpen?: boolean;
@@ -27,47 +27,88 @@ const Modal: React.FC<ModalProps> = ({
   secondaryAction,
   secondaryLabel
 }) => {
-
-  const [showModal, setShowModal] = useState(isOpen)
+  const [showModal, setShowModal] = useState(isOpen);
 
   useEffect(() => {
-    setShowModal(isOpen)
-  }, [isOpen])
+    setShowModal(isOpen);
+  }, [isOpen]);
 
   const handleClose = useCallback(() => {
     if (disabled) {
       return;
     }
 
-    setShowModal(false)
+    setShowModal(false);
     setTimeout(() => {
-      onClose()
-    }, 300)
-  }, [disabled, onClose])
+      onClose();
+    }, 300);
+  }, [disabled, onClose]);
 
   const handleSubmit = useCallback(() => {
     if (disabled) {
       return;
     }
 
-    onSubmit()
-  }, [disabled, onSubmit])
+    onSubmit();
+  }, [disabled, onSubmit]);
 
   const handleSecondaryAction = useCallback(() => {
     if (disabled || !secondaryAction) {
       return;
     }
 
-    secondaryAction()
-  }, [disabled, secondaryAction])
+    secondaryAction();
+  }, [disabled, secondaryAction]);
 
   if (!isOpen) {
     return null;
   }
 
   return (
-    <div></div>
-  )
-}
+    <>
+      <div
+        className="
+          justify-center
+          items-center
+          flex
+          overflow-x-hidden
+          overflow-y-auto
+          fixed
+          inset-0
+          z-50
+          outline-none
+          focus:outline-none
+          bg-neutral-800/70
+        "
+      >
+        <div
+          className="
+            relative
+            w-full
+            md:w-3/4
+            lg:w-3/6
+            xl:w-2/5
+            my-6
+            mx-auto
+            h-full
+            lg:h-auto
+            md:h-auto
+          "
+        >
+          {/* CONTENT */}
+          <div
+            className={`
+              translate
+              duration-300
+              h-full
+              ${showModal ? 'translate-y-0' : 'translate-y-full'}
+              ${showModal ? 'opacity-100' : 'opacity-0'}
+            `}
+          ></div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Modal
+export default Modal;
