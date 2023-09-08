3f32c78c98926b7571482cde0a2bea8eb227973b
Abdul Rosyid <ocritocrit@gmail.com>
2023-09-09 05:53:40 +0700

    add fix hydration component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import NavBar from '@/components/navbar/NavBar';
 import { Nunito } from 'next/font/google';
 import './globals.css';
 import Modal from '@/components/modals/Modal';
+import ClientOnly from '@/components/ClientOnly';
 
 const nunito = Nunito({ subsets: ['latin'] });
 
@@ -14,8 +15,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={nunito.className}>
-        <Modal actionLabel="Submit" secondaryActionLabel="Cancel" title="Modal" isOpen />
-        <NavBar />
+        <ClientOnly>
+          <Modal actionLabel="Submit" secondaryActionLabel="Cancel" title="Modal" isOpen />
+          <NavBar />
+        </ClientOnly>
         {children}
       </body>
     </html>
