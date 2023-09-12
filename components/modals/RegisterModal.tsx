111c6caf23970bba3d56b5acc807319c995c1ccb
Temux Ocit <ocritocrit@gmail.com>
2023-09-12 13:41:46 +0700

    create a footer component for register modal

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -10,6 +10,7 @@ import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
 import { toast } from 'react-hot-toast';
+import Button from '../Button';
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -78,6 +79,56 @@ const RegisterModal = () => {
     </div>
   )
 
+  const footerContent = (
+    <div className='flex flex-col gap-4 mt-3'>
+      <hr />
+      <Button
+        outline
+        label='Continue With Google'
+        icon={FcGoogle}
+        onClick={() => { }}
+      />
+      <Button
+        outline
+        label='Continue With Github'
+        icon={AiFillGithub}
+        onClick={() => { }}
+      />
+      <div
+        className='
+          text-neutral-500
+          text-center
+          mt-4
+          font-light
+        '
+      >
+        <div
+          className='
+            flex
+            flex-row
+            justify-center
+            items-center
+            gap-2
+          '
+        >
+          <div>
+            Already have an account?
+          </div>
+          <div
+            onClick={registerModal.onClose}
+            className='
+              text-neutral-800
+              cursor-pointer
+              hover:underline
+            '
+          >
+            Log in
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
   return (
     <Modal
       disabled={isLoading}
@@ -87,6 +138,7 @@ const RegisterModal = () => {
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   );
 };
