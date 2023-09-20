37b918b06783ee3c092b08f7de95f8b6e475db0f
Temux Ocit <ocritocrit@gmail.com>
2023-09-20 17:49:31 +0700

    change word

--- hooks/useRegisterModal.tsx
+++ hooks/useRegisterModal.tsx
@@ -1,12 +1,12 @@
 import { create } from 'zustand';
 
-interface RegisterModalProps {
+interface RegisterModalStore {
   isOpen: boolean;
   onOpen: () => void;
   onClose: () => void;
 }
 
-const useRegisterModal = create<RegisterModalProps>((set) => ({
+const useRegisterModal = create<RegisterModalStore>((set) => ({
   isOpen: false,
   onOpen: () => set({ isOpen: true }),
   onClose: () => set({ isOpen: false })
