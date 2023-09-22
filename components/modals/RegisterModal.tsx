c30a214888a81a4485c8ca3de4e5e80c58f09477
Temux Ocit <ocritocrit@gmail.com>
2023-09-22 12:49:59 +0700

    remove unecessary

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -3,7 +3,7 @@
 import axios from 'axios';
 import { AiFillGithub } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 import useRegisterModal from '@/hooks/useRegisterModal';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import Modal from './Modal';
