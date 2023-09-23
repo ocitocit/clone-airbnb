151d7f905aaabc4b1128655950bc5c4e8d8aa9e1
Temux Ocit <ocritocrit@gmail.com>
2023-09-23 12:04:29 +0700

    updqte

--- components/CategoryBox.tsx
+++ components/CategoryBox.tsx
@@ -1,6 +1,9 @@
 "use client"
 
+import { useParams, useRouter } from "next/navigation"
+import { useCallback } from "react"
 import { IconType } from "react-icons"
+import qs from "query-string"
 
 interface CategoryBoxProps {
   icon: IconType
@@ -13,27 +16,35 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
   label,
   selected
 }) => {
+  const router = useRouter()
+  const params = useParams()
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {}
+  }, [])
+
+
   return (
-    <div
+    < div
       className={`
-flex
-flex-col
-items-center
-justify-center
-gap-2
-p-3
-border-2
-hover:text-neutral-800
-transition
-cursor-pointer
-${selected ? 'border-b-neutral-800' : 'border-transparent'}
-${selected ? 'text-neutral-800' : 'text-neutral-500'}
-`}>
+        flex
+        flex-col
+        items-center
+        justify-center
+        gap-2
+        p-3
+        border-2
+        hover:text-neutral-800
+        transition
+        cursor-pointer
+        ${selected ? 'border-b-neutral-800' : 'border-transparent'}
+        ${selected ? 'text-neutral-800' : 'text-neutral-500'}
+      `}>
       <Icon size={26} />
       <div className="font-medium text-sm">
         {label}
       </div>
-    </div>
+    </div >
   )
 }
 
