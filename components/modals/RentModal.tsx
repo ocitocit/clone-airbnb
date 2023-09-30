c2236e2f1a0f8402784c4973483235c169f29ba0
Temux Ocit <ocritocrit@gmail.com>
2023-09-30 17:50:25 +0700

    update rent

--- components/modals/RentModal.tsx
+++ components/modals/RentModal.tsx
@@ -1,10 +1,45 @@
 "use client"
 
+import { useMemo, useState } from "react"
 import useRentModal from "@/hooks/useRentModal"
 import Modal from "./Modal"
 
+enum STEP {
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGES = 3,
+  DESCRIPTION = 4,
+  PRICE = 5
+}
+
 const RentModal = () => {
   const rentModal = useRentModal()
+  const [step, setStep,] = useState(STEP.CATEGORY)
+
+  const onBack = () => {
+    setStep((value) => value - 1)
+  }
+
+  const onNext = () => {
+    setStep((value) => value + 1)
+  }
+
+  const actionLabel = useMemo(() => {
+    if (step === STEP.PRICE) {
+      return 'Create'
+    }
+
+    return 'Next'
+  }, [step])
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEP.CATEGORY) {
+      return undefined
+    }
+
+    return 'Back'
+  }, [step])
 
   return (
     <Modal
@@ -12,7 +47,8 @@ const RentModal = () => {
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
       onSubmit={rentModal.onClose}
-      actionLabel="Submit"
+      actionLabel={actionLabel}
+      secondaryActionLabel={secondaryActionLabel}
     />
   )
 }
