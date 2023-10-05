f9a5f9153727d8db39f7f3391fe5c3bc23e26972
Abdul Rosyid <ocritocrit@gmail.com>
2023-10-05 23:27:57 +0700

    showing image on listing card

--- components/listings/ListingCard.tsx
+++ components/listings/ListingCard.tsx
@@ -1,19 +1,21 @@
-'use client'
+'use client';
 
-import useCountries from "@/hooks/useCountries"
-import { SafeUser } from "@/types"
-import { Listing, Reservation } from "@prisma/client"
-import { useRouter } from "next/navigation"
-import { useCallback, useMemo } from "react"
+import useCountries from '@/hooks/useCountries';
+import { SafeUser } from '@/types';
+import { Listing, Reservation } from '@prisma/client';
+import { format } from 'date-fns';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { useCallback, useMemo } from 'react';
 
 interface ListingCardProps {
-  data: Listing
-  reservation?: Reservation
-  onAction?: (id: string) => void
-  disabled?: boolean
-  actionLabel?: string
-  actionId?: string
-  currentUser?: SafeUser | null
+  data: Listing;
+  reservation?: Reservation;
+  onAction?: (id: string) => void;
+  disabled?: boolean;
+  actionLabel?: string;
+  actionId?: string;
+  currentUser?: SafeUser | null;
 }
 
 const ListingCard: React.FC<ListingCardProps> = ({
@@ -22,47 +24,77 @@ const ListingCard: React.FC<ListingCardProps> = ({
   onAction,
   disabled,
   actionLabel,
-  actionId = "",
+  actionId = '',
   currentUser
 }) => {
-  const router = useRouter()
-  const { getByValue } = useCountries()
+  const router = useRouter();
+  const { getByValue } = useCountries();
 
-  const location = getByValue(data.locationValue)
+  const location = getByValue(data.locationValue);
 
-  const handleCancel = useCallback((
-    e: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    e.stopPropagation()
+  const handleCancel = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
 
-    if (disabled) {
-      return
-    }
+      if (disabled) {
+        return;
+      }
 
-    onAction?.(actionId)
-  }, [disabled, onAction, actionId])
+      onAction?.(actionId);
+    },
+    [disabled, onAction, actionId]
+  );
 
   const price = useMemo(() => {
     if (reservation) {
-      return reservation.totalPrice
+      return reservation.totalPrice;
     }
 
-    return data.price
-  }, [reservation, data.price])
+    return data.price;
+  }, [reservation, data.price]);
 
   const reservationDate = useMemo(() => {
     if (!reservation) {
-      return null
+      return null;
     }
 
-    const start = new Date(reservation.startDate)
-    const end = new Date(reservation.endDate)
+    const start = new Date(reservation.startDate);
+    const end = new Date(reservation.endDate);
 
-  })
+    return `${format(start, 'PP')} - ${format(end, 'PP')}`;
+  }, [reservation]);
 
   return (
-    <div>ListingCard</div>
-  )
-}
+    <div
+      onClick={() => router.push(`/listings/${data.id}`)}
+      className="group col-span-1 cursor-pointer"
+    >
+      <div className="flex w-full flex-col gap-2">
+        <div
+          className="
+            relative
+            aspect-square
+            w-full
+            overflow-hidden
+            rounded-xl
+          "
+        >
+          <Image
+            fill
+            alt="Listing"
+            src={data.imageSrc}
+            className="
+              h-full
+              w-full
+              object-cover
+              transition
+              group-hover:scale-110
+            "
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ListingCard
+export default ListingCard;
