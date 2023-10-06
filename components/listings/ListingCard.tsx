d67a3abed4c6c1210cb2de86482f1064c3e7e3ab
Abdul Rosyid <ocritocrit@gmail.com>
2023-10-06 08:32:39 +0700

    update listingcard

--- components/listings/ListingCard.tsx
+++ components/listings/ListingCard.tsx
@@ -8,6 +8,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useCallback, useMemo } from 'react';
 import HeartButton from '../HeartButton';
+import Button from '../Button';
 
 interface ListingCardProps {
   data: Listing;
@@ -96,6 +97,17 @@ const ListingCard: React.FC<ListingCardProps> = ({
             <HeartButton listingId={data.id} currentUser={currentUser} />
           </div>
         </div>
+        <div className="text-lg font-semibold">
+          {location?.region}, {location?.label}
+        </div>
+        <div className="font-light text-neutral-500">{reservationDate || data.category}</div>
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semibold">$ {price}</div>
+          {!reservation && <div className="font-light">night</div>}
+        </div>
+        {onAction && actionLabel && (
+          <Button disabled={disabled} small label={actionLabel} onClick={handleCancel} />
+        )}
       </div>
     </div>
   );
