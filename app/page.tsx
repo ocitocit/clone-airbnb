83c3a1514f762c28003db8d2a1eafcfe63023a49
Abdul Rosyid <ocritocrit@gmail.com>
2023-10-06 08:40:32 +0700

    forgot add gap

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,24 @@
-import Container from "@/components/Container";
-import EmptyState from "@/components/EmptyState";
-import getListings from "./actions/getListings";
-import ListingCard from "@/components/listings/ListingCard";
-import getCurrentUser from "./actions/getCurrentUser";
+import Container from '@/components/Container';
+import EmptyState from '@/components/EmptyState';
+import getListings from './actions/getListings';
+import ListingCard from '@/components/listings/ListingCard';
+import getCurrentUser from './actions/getCurrentUser';
 
 export default async function Home() {
-  const listings = await getListings()
-  const currentUser = await getCurrentUser()
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
-    return (
-      <EmptyState showReset />
-    )
+    return <EmptyState showReset />;
   }
   return (
     <Container>
       <div
         className="
-          pt-24
           grid
           grid-cols-1
+          gap-8
+          pt-24
           sm:grid-cols-2
           md:grid-cols-3
           lg:grid-cols-4
@@ -28,15 +27,9 @@ export default async function Home() {
         "
       >
         {listings.map((listing: any) => {
-          return (
-            <ListingCard
-              key={listing.id}
-              data={listing}
-              currentUser={currentUser}
-            />
-          )
+          return <ListingCard key={listing.id} data={listing} currentUser={currentUser} />;
         })}
       </div>
     </Container>
-  )
+  );
 }
