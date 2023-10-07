8773c221c1d72bfa1b10343de34cdd1dcdb14ca5
Abdul Rosyid <ocritocrit@gmail.com>
2023-10-07 09:34:00 +0700

    use action on this server page

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,5 +1,21 @@
-const ListingPage = () => {
-  return <div>individual page</div>;
+import getCurrentUser from '@/app/actions/getCurrentUser';
+import getListingById from '@/app/actions/getListingById';
+import EmptyState from '@/components/EmptyState';
+import ListingClient from './ListingClient';
+
+interface IParams {
+  listingId?: string;
+}
+
+const ListingPage = async ({ params }: { params: IParams }) => {
+  const listing = await getListingById(params);
+  const currentUser = await getCurrentUser();
+
+  if (!listing) {
+    return <EmptyState />;
+  }
+
+  return <ListingClient listing={listing} currentUser={currentUser} />;
 };
 
 export default ListingPage;
