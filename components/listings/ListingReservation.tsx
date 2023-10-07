639e1b07136657fbbb687fd2ceea0f6800729994
Abdul Rosyid <ocritocrit@gmail.com>
2023-10-07 17:50:22 +0700

    create new ListingReservation compt

---
+++ components/listings/ListingReservation.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+interface ListingReservationProps {
+  price: number;
+  dateRange: Range;
+  totalPrice: number;
+  onChangeDate: (value: Range) => void;
+  onSubmit: () => void;
+  disabled?: boolean;
+  disableDates: Date[];
+}
+
+const ListingReservation: React.FC<ListingReservationProps> = ({
+  price,
+  dateRange,
+  totalPrice,
+  onChangeDate,
+  onSubmit,
+  disabled,
+  disableDates
+}) => {
+  return <div>ListingReservation</div>;
+};
+
+export default ListingReservation;
