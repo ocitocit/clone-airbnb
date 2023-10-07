8a7a2162deb6f1033b5ebc25fb3a65a152ec1f2a
Abdul Rosyid <ocritocrit@gmail.com>
2023-10-07 11:09:50 +0700

    update

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -2,6 +2,7 @@
 
 import Container from '@/components/Container';
 import ListingHead from '@/components/listings/ListingHead';
+import ListingInfo from '@/components/listings/ListingInfo';
 import { categories } from '@/components/navbar/Categories';
 import { SafeListing, SafeUser } from '@/types';
 import { Reservation } from '@prisma/client';
@@ -30,6 +31,25 @@ const ListingClient: React.FC<ListingClientProps> = ({ listing, currentUser }) =
             id={listing.id}
             currentUser={currentUser}
           />
+          <div
+            className="
+            mt-6
+            grid
+            grid-cols-1
+            md:grid-cols-7
+            md:gap-10
+            "
+          >
+            <ListingInfo
+              user={listing.user}
+              category={category}
+              description={listing.description}
+              roomCount={listing.roomCount}
+              guestCount={listing.guestCount}
+              bathroomCount={listing.bathroomCount}
+              locationValue={listing.locationValue}
+            />
+          </div>
         </div>
       </div>
     </Container>
