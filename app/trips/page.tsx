cb09b7cd0f19a345724432d9a38c84968807f143
Abdul Rosyid <ocritocrit@gmail.com>
2023-10-14 07:39:27 +0700

    create trips page

---
+++ app/trips/page.tsx
@@ -0,0 +1,27 @@
+import getCurrentUser from '@/actions/getCurrentUser';
+import getReservations from '@/actions/getReservations';
+import EmptyState from '@/components/EmptyState';
+
+const TripsPage = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return <EmptyState title="Unauthorized" subTitle="Please login" />;
+  }
+
+  const reservations = await getReservations({
+    userId: currentUser.id
+  });
+
+  if (reservations.length === 0) {
+    return (
+      <EmptyState title="No trips found" subTitle="Looks like you havent reserved any trips" />
+    );
+  }
+
+  return (
+    <>
+      <TripsClient reservations={reservations} currentUser={currentUser} />
+    </>
+  );
+};
