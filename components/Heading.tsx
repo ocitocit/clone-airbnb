abef986c04d02a75c11aeca57fb1cdb872aeab4f
Abdul Rosyid <ocritocrit@gmail.com>
2023-10-15 11:14:30 +0700

    fix style

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -8,7 +8,7 @@ interface HeadingProps {
 
 const Heading: React.FC<HeadingProps> = ({ title, subTitle, center }) => {
   return (
-    <div className={center ? 'text-current' : 'text-start'}>
+    <div className={center ? 'text-center' : 'text-start'}>
       <div className="text-2xl font-bold">{title}</div>
       <div className="mt-2 font-light text-neutral-500">{subTitle}</div>
     </div>
