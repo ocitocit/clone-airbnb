984d7f9f13db7f02f77cc041486e5777c0912174
Temux Ocit <ocritocrit@gmail.com>
2023-10-16 15:38:01 +0700

    update

--- actions/getListings.ts
+++ actions/getListings.ts
@@ -1,8 +1,24 @@
 import prisma from '@/libs/prismadb';
 
-export default async function getListings() {
+export interface IListingParams{
+  userId?:string
+}
+
+export default async function getListings(
+  params:IListingParams
+) {
+
+  const {userId}= params
+
+  let query:any={}
+
+  if (userId){
+    query.userId=userId
+  }
+
   try {
     const listings = await prisma.listing.findMany({
+      where:query,
       orderBy: {
         createdAt: 'desc'
       }
