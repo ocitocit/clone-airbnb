c4283569417eccec8e55154a38a97978712fbf39
Abdul Rosyid <ocritocrit@gmail.com>
2023-10-18 06:29:51 +0700

    indent

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -6,11 +6,11 @@ const Loader = () => {
   return (
     <div
       className="
-      h-[70vh]
-      flex
-      flex-col
-      justify-center
-      items-center
+        h-[70vh]
+        flex
+        flex-col
+        justify-center
+        items-center
       "
     >
       <PuffLoader
