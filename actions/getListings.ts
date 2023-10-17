d57aed495fab49ca45fa8b7fa65a3e8152411900
Abdul Rosyid <ocritocrit@gmail.com>
2023-10-17 07:44:35 +0700

    update for search and category

--- actions/getListings.ts
+++ actions/getListings.ts
@@ -1,24 +1,85 @@
 import prisma from '@/libs/prismadb';
 
-export interface IListingParams{
-  userId?:string
+export interface IListingParams {
+  userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
 }
 
 export default async function getListings(
-  params:IListingParams
+  params: IListingParams
 ) {
 
-  const {userId}= params
+  const {
+    userId,
+    roomCount,
+    guestCount,
+    bathroomCount,
+    startDate,
+    endDate,
+    locationValue,
+    category
+  } = params;
 
-  let query:any={}
+  let query: any = {};
 
-  if (userId){
-    query.userId=userId
+  if (userId) {
+    query.userId = userId;
+  }
+
+  if (category) {
+    query.category = category;
+  }
+
+  if (roomCount) {
+    query.roomCount = {
+      gte: +roomCount
+    };
+  }
+
+  if (guestCount) {
+    query.guestCount = {
+      gte: +guestCount
+    };
+  }
+
+  if (bathroomCount) {
+    query.bathroomCount = {
+      gte: +bathroomCount
+    };
+  }
+
+  if (locationValue) {
+    query.locationValue = locationValue;
+  }
+
+  if (startDate && endDate) {
+    query.NOT = {
+      reservations: {
+        some: {
+          OR: [
+            {
+              endDate: { gte: startDate },
+              startDate: { lte: startDate }
+            },
+            {
+              startDate: { lte: endDate },
+              endDate: { gte: endDate }
+            }
+          ]
+        }
+      }
+    }
   }
 
   try {
     const listings = await prisma.listing.findMany({
-      where:query,
+      where: query,
       orderBy: {
         createdAt: 'desc'
       }
@@ -32,5 +93,5 @@ export default async function getListings(
     return safeListings;
   } catch (error: any) {
     throw new Error(error);
-  }
+  };
 }
